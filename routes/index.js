c4e5e32d0c44312dbb9ed74fa253976173ca6f62
Pramod shah <pramodshah1444@gmail.com>
2020-08-20 14:40:19 +0530

    Third Commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var fetch = require('node-fetch');
 
 
 router.get('/',(req,res)=>{
-    fetch('http://www.omdbapi.com/?s=batman&apikey=9d0e7859')
+    fetch('http://www.omdbapi.com/?s=action&apikey=9d0e7859')
     .then(res => res.json())
     .then(json => {
         var result = [];
@@ -12,21 +12,50 @@ router.get('/',(req,res)=>{
         keys.forEach(function(key){
             result.push(json[key]);
         });
-        console.log(result);
         res.render('index',{movies:result});
     });
     
     
 });
 
-router.get('/movie/:id',(req,res)=>{
-    var id= req.params.id
-    
-    fetch('http://www.omdbapi.com/?t=id&apikey=9d0e7859')
+router.get('/genre/:category',(req,res)=>{
+    var category = req.params.category;
+    var url = "http://www.omdbapi.com/?apikey=9d0e7859&s="+category;
+    fetch(url)
     .then(res => res.json())
     .then(json => {
-        console.log(json);
+        var result = [];
+        var keys = Object.keys(json);
+        keys.forEach(function(key){
+            result.push(json[key]);
+        });
+        res.render('index',{movies:result});
+    });
+    
+    
+});
+
+router.get('/:id',(req,res)=>{
+    var id = req.params.id;
+    var url = "http://www.omdbapi.com/?apikey=9d0e7859&i=" + id;
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        
         res.render('view-movie',{movie:json});
+    });
+    
+    
+});
+
+router.post('/search',(req,res)=>{
+    var title = req.body.title;
+    console.log(title);
+    var url = "http://www.omdbapi.com/?apikey=9d0e7859&t="+title;
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        res.render('search-result',{movie:json});
     });
     
     
